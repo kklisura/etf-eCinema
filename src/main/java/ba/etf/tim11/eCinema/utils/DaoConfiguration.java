cb6994cd537dfbfc95a4d64ab914c275a5864905
Kenan Klisura <kklisura@hotmail.com>
2013-11-19 12:50:46 +0100

    Added initial DAO configuration class.

---
+++ src/main/java/ba/etf/tim11/eCinema/utils/DaoConfiguration.java
@@ -0,0 +1,55 @@
+package ba.etf.tim11.eCinema.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class DaoConfiguration
+{
+	// Constants ----------------------------------------------------------------------
+	
+	public static final String CONFIGURATION_FILE = "dao.configuration";
+	private static final Properties configuration = new Properties();
+	
+	
+	static {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream configurationFile = classLoader.getResourceAsStream(CONFIGURATION_FILE);
+
+        if (configurationFile == null) {
+        	// TODO(kklisura): Create new exception type
+            //throw new Exception("Configuration file '" + CONFIGURATION_FILE + "' is missing in classpath.");
+        }
+
+        try {
+        	configuration.load(configurationFile);
+        } catch (IOException e) {
+        	// TODO(kklisura): 
+            //throw new Exception("Cannot load properties file '" + CONFIGURATION_FILE + "'.", e);
+        }
+	}
+	
+	
+	public DaoConfiguration() throws Exception 
+	{
+		
+	}
+
+    public String getEntry(String key, boolean mandatory) throws Exception 
+    {
+        String entry = configuration.getProperty(key);
+
+        if (entry == null || entry.trim().length() == 0) 
+        {
+            if (mandatory) {
+                throw new Exception("Required property '" + key + "'" + " is missing in configuration file '" + CONFIGURATION_FILE + "'.");
+            } else {
+                entry = null;
+            }
+        }
+
+        return entry;
+    }
+	
+}
