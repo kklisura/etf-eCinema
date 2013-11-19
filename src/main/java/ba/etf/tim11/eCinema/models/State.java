8fe46a9ef0c059938333af47ffdad5216851a319
Naida Huseinovic <naida.durmo@gmail.com>
2013-11-19 14:39:02 +0100

    Edit State

--- src/main/java/ba/etf/tim11/eCinema/models/State.java
+++ src/main/java/ba/etf/tim11/eCinema/models/State.java
@@ -1,5 +1,62 @@
 package ba.etf.tim11.eCinema.models;
 
-public class State {
+import java.util.Date;
+
+public class State 
+{
+	int id;
+	String name;
+	String shortName;
+	Date updatedAt;
+	Date createdAt;
+	
+	
+	public State(int id, String name, String shortName, Date updatedAt,
+				Date createdAt) {
+		this.id = id;
+		this.name = name;
+		this.shortName = shortName;
+		this.updatedAt = updatedAt;
+		this.createdAt = createdAt;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getShortName() {
+		return shortName;
+	}
+	
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
