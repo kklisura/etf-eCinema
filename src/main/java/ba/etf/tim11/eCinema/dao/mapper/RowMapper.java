5e2a39dca8dfde7158ba1e96996d5678ebca97e8
Maida Kapic <m_kapic@hotmail.com>
2013-11-20 12:51:55 +0100

    added UserRowMapper

--- src/main/java/ba/etf/tim11/eCinema/dao/mapper/RowMapper.java
+++ src/main/java/ba/etf/tim11/eCinema/dao/mapper/RowMapper.java
@@ -1,9 +1,10 @@
 package ba.etf.tim11.eCinema.dao.mapper;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public interface RowMapper
 {
-    public Object map(ResultSet rs);
+    public Object map(ResultSet rs) throws SQLException;
 }
--- src/main/java/ba/etf/tim11/eCinema/dao/mapper/UserRowMapper.java
+++ src/main/java/ba/etf/tim11/eCinema/dao/mapper/UserRowMapper.java
@@ -1,12 +1,18 @@
 package ba.etf.tim11.eCinema.dao.mapper;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ba.etf.tim11.eCinema.models.User;
 
 public class UserRowMapper implements RowMapper {
 
 	@Override
-	public Object map(ResultSet rs) {
-		// TODO Auto-generated method stub
+	public Object map(ResultSet rs) throws SQLException {
+		
+		User user = new User();
+		
+		user.setId(rs.getInt(1));
 		return null;
 	}
 
