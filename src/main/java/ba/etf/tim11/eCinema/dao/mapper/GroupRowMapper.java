e356d29c41585f55a864100cfc81033bafa45001
Naida Huseinovic <naida.durmo@gmail.com>
2013-11-20 14:32:59 +0100

    Added something new

--- src/main/java/ba/etf/tim11/eCinema/dao/mapper/GroupRowMapper.java
+++ src/main/java/ba/etf/tim11/eCinema/dao/mapper/GroupRowMapper.java
@@ -14,13 +14,9 @@ public class GroupRowMapper implements RowMapper {
 		Group group = new Group();
 		
 		group.setId(rs.getInt(1));
-		
 		group.setName(rs.getString(2));
-		
-		group.setDescription(rs.getString(3));
-		
-		group.setUpdatedAt(rs.getDate(4));
-		
+		group.setDescription(rs.getString(3));		
+		group.setUpdatedAt(rs.getDate(4));		
 		group.setCreatedAt(rs.getDate(5));
 		
 		return null;
