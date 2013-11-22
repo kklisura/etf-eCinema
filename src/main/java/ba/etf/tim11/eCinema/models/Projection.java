68ba3bae6e7e44f2782b1e3996ad71c0af14afa8
Maida Kapic <m_kapic@hotmail.com>
2013-11-22 23:13:02 +0100

    Added classes: Cinema, CinemaHoll, Projction, ProjectionType, Receipt, Reservation, ReservationType, Seat

---
+++ src/main/java/ba/etf/tim11/eCinema/models/Projection.java
@@ -0,0 +1,94 @@
+package ba.etf.tim11.eCinema.models;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Projection
+{
+	int id;
+	Date time;
+	BigDecimal pricePerSeat;
+	Content content;
+	CinemaHoll cinemaHoll;
+	ProjectionType projectionType;
+	Date updatedAt;
+	Date createdAt;
+	
+	public Projection(int id, Date time, BigDecimal pricePerSeat, Content content,
+			CinemaHoll cinemaHoll, ProjectionType projectionType,
+			Date updatedAt, Date createdAt)
+	{
+		this.id = id;
+		this.time = time;
+		this.pricePerSeat = pricePerSeat;
+		this.content = content;
+		this.cinemaHoll = cinemaHoll;
+		this.projectionType = projectionType;
+		this.updatedAt = updatedAt;
+		this.createdAt = createdAt;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Date getTime() {
+		return time;
+	}
+	
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	public BigDecimal getPricePerSeat() {
+		return pricePerSeat;
+	}
+	
+	public void setPricePerSeat(BigDecimal pricePerSeat) {
+		this.pricePerSeat = pricePerSeat;
+	}
+	
+	public Content getContent() {
+		return content;
+	}
+	
+	public void setContent(Content content) {
+		this.content = content;
+	}
+	
+	public CinemaHoll getCinemaHoll() {
+		return cinemaHoll;
+	}
+	
+	public void setCinemaHoll(CinemaHoll cinemaHoll) {
+		this.cinemaHoll = cinemaHoll;
+	}
+	
+	public ProjectionType getProjectionType() {
+		return projectionType;
+	}
+	
+	public void setProjectionType(ProjectionType projectionType) {
+		this.projectionType = projectionType;
+	}
+	
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+}
---
+++ src/main/java/ba/etf/tim11/eCinema/models/Reservation.java
@@ -0,0 +1,83 @@
+package ba.etf.tim11.eCinema.models;
+
+import java.util.Date;
+
+public class Reservation
+{
+	int id;
+	Projection projection;
+	User user;
+	Receipt receipt;
+	ReservationType reservationType;
+	Date createdAt;
+	Date updatedAt;
+	
+	public Reservation(int id, Projection projection, User user,
+			Receipt receipt, ReservationType reservationType, Date createdAt,
+			Date updatedAt) 
+	{
+		this.id = id;
+		this.projection = projection;
+		this.user = user;
+		this.receipt = receipt;
+		this.reservationType = reservationType;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Projection getProjection() {
+		return projection;
+	}
+	
+	public void setProjection(Projection projection) {
+		this.projection = projection;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Receipt getReceipt() {
+		return receipt;
+	}
+	
+	public void setReceipt(Receipt receipt) {
+		this.receipt = receipt;
+	}
+	
+	public ReservationType getReservationType() {
+		return reservationType;
+	}
+	
+	public void setReservationType(ReservationType reservationType) {
+		this.reservationType = reservationType;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
